c4f2831d977a8f43a48c35144868d08d14e1591a
foggy-19 <milan.pasic@panonit.com>
2026-07-09 19:01:28 +0200

    rereview initial setup

---
+++ rereview/src/main/java/com/panonit/rereview/RereviewApplication.java
@@ -0,0 +1,13 @@
+package com.panonit.rereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RereviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RereviewApplication.class, args);
+    }
+
+}
---
+++ rereview/src/test/java/com/panonit/rereview/RereviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panonit.rereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RereviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
