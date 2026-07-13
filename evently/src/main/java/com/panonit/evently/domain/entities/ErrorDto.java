a2880f9ac22b02820e2f352c712f083dd577504c
foggy-19 <milan.pasic@panonit.com>
2026-07-13 19:18:29 +0200

    initial setup for `evently` app

---
+++ evently/src/main/java/com/panonit/evently/domain/entities/ErrorDto.java
@@ -0,0 +1,17 @@
+package com.panonit.evently.domain.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ErrorDto {
+
+    private int status;
+
+    private String message;
+}
---
+++ evently/src/test/java/com/panonit/evently/EventlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panonit.evently;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
